442788bd546ef542c70bfb7a58fdbc05f24259d5
Dullah Mtavya <mtavya420@gmail.com>
2023-03-02 00:48:03 +0900

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  AiFillInstagram,
+  AiOutlineTwitter,
+  AiFillFacebook,
+} from "react-icons/ai";
+
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <p>2023 KoreaTech Products All rights Reserved</p>
+
+      <p className="icons">
+        <AiFillInstagram/>
+        <AiOutlineTwitter/>
+        <AiFillFacebook/>
+
+      </p>
+    </div>
+  );
+}
+
+export default Footer
---
+++ sanity_koreatech/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'ehbn9tb1',
+    dataset: 'production'
+  }
+})
