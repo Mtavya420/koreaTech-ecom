c3da64183bdf46d525f50c9ff74e6065495e62a8
Dullah Mtavya <mtavya420@gmail.com>
2023-03-16 14:11:30 +0900

    donwload file highlighted

--- src/pages/downloadFile.jsx
+++ src/pages/downloadFile.jsx
@@ -1,38 +1,38 @@
 import React from "react";
 import { CSVLink } from "react-csv";
 
-const headers = [
-  { label: "Name", key: "name" },
-  { label: "Price", key: "price" },
-  { label: "Quantity", key: "quantity" },
-];
+// const headers = [
+//   { label: "Name", key: "name" },
+//   { label: "Price", key: "price" },
+//   { label: "Quantity", key: "quantity" },
+// ];
 
-const data = [
+// const data = [
  
-];
+// ];
 
-const csvReport = {
-  data: data,
-  headers: headers,
-  filename: "Clue_Mediator_Report.csv",
-};
+// const csvReport = {
+//   data: data,
+//   headers: headers,
+//   filename: "Clue_Mediator_Report.csv",
+// };
 
-function App() {
-  return (
-    <div classname="App">
-      <h3>
-        Export data to CSV in React -{" "}
-        <a
-          href="https://cluemediator.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Clue Mediator
-        </a>
-      </h3>
-      <csvlink {...csvreport}>Export to CSV</csvlink>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div classname="App">
+//       <h3>
+//         Export data to CSV in React -{" "}
+//         <a
+//           href="https://cluemediator.com"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Clue Mediator
+//         </a>
+//       </h3>
+//       <csvlink {...csvreport}>Export to CSV</csvlink>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
